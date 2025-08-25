bb3b20cd9c9acee1a663be9c34d380abfa480cb7
Alex Lesnoy <123098237+bebelekele@users.noreply.github.com>
2025-08-25 16:09:25 +0300

    Update: added error catcher

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -13,20 +13,51 @@ export const TodoModal: React.FC<Props> = ({ todo, deleteTodo }) => {
   const [loader, setLoader] = useState(true);
   const [user, setUser] = useState<User>({} as User);
 
+  const [error, setError] = useState<string | null>(null);
+
   useEffect(() => {
-    getUser(todo.userId).then(u => {
-      setUser(u);
-      setLoader(false);
-    });
+    setError('');
+    getUser(todo.userId)
+      .then(u => {
+        setUser(u);
+      })
+      .catch(() => setError('Failed to load user'))
+      .finally(() => setLoader(false));
   }, [todo.userId]);
 
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
+      {loader && <Loader />}
 
-      {loader ? (
-        <Loader />
-      ) : (
+      {!loader && error && (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              // eslint-disable-next-line max-len
+              className="modal-card-title has-text-weight-medium has-text-danger"
+              data-cy="modal-header"
+            >
+              Помилка
+            </div>
+
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => {
+                deleteTodo(null);
+              }}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p style={{ color: 'red' }}>{error}</p>
+          </div>
+        </div>
+      )}
+
+      {!loader && !error && (
         <div className="modal-card">
           <header className="modal-card-head">
             <div
